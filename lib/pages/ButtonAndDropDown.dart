425c7e83ebe3a1ca5ec66013f93346706b5c7eef
m-nahrowi <nahrowi.one@gmail.com>
2023-12-30 08:36:47 +0700

    add "Menambahkan file baru u/ latihan"

---
+++ lib/pages/ButtonAndDropDown.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+class ButtonDropDown extends StatelessWidget {
+  const ButtonDropDown({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Button dan Dropdown"),
+      ),
+      body: Container(
+        margin: EdgeInsets.all(5.0),
+        child: Column(
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.all(16.0),
+              padding: EdgeInsets.all(10.0),
+              child: Column(
+                children: [
+                  Text("ini adalah elevated button"),
+                  ElevatedButton(
+                      onPressed: () {}, child: const Text("ElevantedButton")),
+                ],
+              ),
+            ),
+            Container(
+                child: Column(
+              children: [
+                Text("ini adalah text button"),
+                TextButton(onPressed: () {}, child: const Text("Text Button"))
+              ],
+            )),
+            Container(
+              padding: EdgeInsets.all(10.0),
+              margin: EdgeInsets.all(10.0),
+              child: Column(
+                children: [
+                  Text("Ini adalah OutlineButton"),
+                  OutlinedButton(
+                      onPressed: () {}, child: Text("outlineButton")),
+                ],
+              ),
+            ),
+            Container(
+              child: Column(
+                children: [
+                  Text("IconButton"),
+                  IconButton(
+                    onPressed: () {
+                      // aksi saat button ditekan
+                    },
+                    icon: const Icon(Icons.volume_up),
+                    tooltip: 'Increase volume by 10',
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              child: Column(
+                children: [
+                  Text('ini drop down, (ongoing)'),
+                  // klik tombol untuk ke page dropdown
+                  ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => ButtonDropDown()));
+                      },
+                      child: Text(("Pindah ke latihan Button")))
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/DetailScreen.dart
+++ lib/pages/DetailScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'ButtonAndDropDown.dart';
 
 void main() => runApp(DetailScreen());
 
@@ -24,7 +25,7 @@ class DetailScreen extends StatelessWidget {
               ),
             ),
             Container(
-              margin: const EdgeInsets.symmetric(vertical : 16.0),
+              margin: const EdgeInsets.symmetric(vertical: 16.0),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: <Widget>[
@@ -35,20 +36,43 @@ class DetailScreen extends StatelessWidget {
                       Text('Open Every'),
                     ],
                   ),
-                   Column(
+                  Column(
                     children: const <Widget>[
                       Icon(Icons.access_alarm),
                       SizedBox(height: 8.0),
                       Text('09:00 - 20:00'),
                     ],
                   ),
-                   Column(
+                  Column(
                     children: const <Widget>[
-                      Icon(Icons.money_off_csred),
+                      Icon(Icons.attach_money_sharp),
                       SizedBox(height: 8.0),
-                      Text('Open Everyday'),
+                      Text('Rp. 25.000'),
                     ],
                   ),
+                ],
+              ),
+            ),
+            Container(
+                padding: const EdgeInsets.all(16.0),
+                child: const Text(
+                  "Berada di jalur utama Bandung-Lembang, Farm House menjadi objek wisata yang tidak pernah sepi pengunjung. Selain karena letaknya strategis, kawasan ini juga menghadirkan nuansa wisata khas Eropa. Semua itu diterapkan dalam bentuk spot swafoto Instagramable.",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 16.0),
+                )),
+            Container(
+              padding: EdgeInsets.all(10.0),
+              child: Column(
+                children: <Widget>[
+                  Text("ini untuk ke Button"),
+                  ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => ButtonDropDown()));
+                      },
+                      child: Text(("Pindah ke latihan Button")))
                 ],
               ),
             )
