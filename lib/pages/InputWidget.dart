3ae184d9d2ac416e3f68cd2e341e2385f5322d39
m-nahrowi <nahrowi.one@gmail.com>
2023-12-30 22:10:42 +0700

    add "membuat TextInput"

--- lib/pages/InputWidget.dart
+++ lib/pages/InputWidget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 
+
 class InputWidget extends StatefulWidget {
   const InputWidget({super.key});
 
@@ -9,6 +10,8 @@ class InputWidget extends StatefulWidget {
 }
 
 class _InputWidgetState extends State<InputWidget> {
+  String _name = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,12 +19,43 @@ class _InputWidgetState extends State<InputWidget> {
         title: const Text("Input Widget"),
       ),
       body: Container(
-        margin: EdgeInsets.all(10.0),
+        margin: EdgeInsets.all(5.0),
         child: Column(
           children: <Widget>[
             Text('Input widget'),
+            // Input latihan
+
+            Container(
+              child: Column(
+                children: [
+                  Text('ini textfield biasa'),
+                  TextField(
+                    decoration: const InputDecoration(
+                      hintText: 'write yourname here...',
+                      labelText: 'Your Name',
+                    ),
+                    onChanged: (String value) {
+                      setState(() {
+                        _name = value;
+                      });
+                    },
+                  ),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                      onPressed: () {
+                        showDialog(
+                            context: context,
+                            builder: (context) {
+                              return AlertDialog(
+                                content: Text('Hello, $_name'),
+                              );
+                            });
+                      },
+                      child: const Text('submit'))
+                ],
+              ),
+            )
           ],
-          
         ),
       ),
     );
