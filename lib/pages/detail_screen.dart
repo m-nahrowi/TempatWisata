eddfbf8a62fd8948f4b6862aaecebed8f7a01652
m-nahrowi <nahrowi.one@gmail.com>
2024-01-02 02:48:30 +0700

    add "memperbaiki tampilan"

--- lib/pages/detail_screen.dart
+++ lib/pages/detail_screen.dart
@@ -12,104 +12,142 @@ class DetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Container(
-              margin: const EdgeInsets.only(top: 16.0),
-              child: const Text(
-                'Farm House Lembang',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontFamily: 'Oswald',
-                  fontSize: 30.0,
-                  fontWeight: FontWeight.bold,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Image.asset('images/wisata.jpg'),
+              Container(
+                margin: const EdgeInsets.only(top: 16.0),
+                child: const Text(
+                  'Farm House Lembang',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontFamily: 'Oswald',
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
-            ),
-            Container(
-              margin: const EdgeInsets.symmetric(vertical: 16.0),
-              child: const Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Column(
-                    children: <Widget>[
-                      Icon(Icons.calendar_today),
-                      SizedBox(height: 8.0),
-                      Text('Open Every'),
-                    ],
-                  ),
-                  Column(
-                    children: <Widget>[
-                      Icon(Icons.access_alarm),
-                      SizedBox(height: 8.0),
-                      Text('09:00 - 20:00'),
-                    ],
-                  ),
-                  Column(
-                    children: <Widget>[
-                      Icon(Icons.attach_money_sharp),
-                      SizedBox(height: 8.0),
-                      Text('Rp. 25.000'),
-                    ],
-                  ),
-                ],
+              Container(
+                margin: const EdgeInsets.symmetric(vertical: 16.0),
+                child: const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Column(
+                      children: <Widget>[
+                        Icon(Icons.calendar_today),
+                        SizedBox(height: 8.0),
+                        Text('Open Every'),
+                      ],
+                    ),
+                    Column(
+                      children: <Widget>[
+                        Icon(Icons.access_alarm),
+                        SizedBox(height: 8.0),
+                        Text('09:00 - 20:00'),
+                      ],
+                    ),
+                    Column(
+                      children: <Widget>[
+                        Icon(Icons.attach_money_sharp),
+                        SizedBox(height: 8.0),
+                        Text('Rp. 25.000'),
+                      ],
+                    ),
+                  ],
+                ),
               ),
-            ),
-            Container(
+              Container(
                 padding: const EdgeInsets.all(16.0),
                 child: const Text(
                   "Berada di jalur utama Bandung-Lembang, Farm House menjadi objek wisata yang tidak pernah sepi pengunjung. Selain karena letaknya strategis, kawasan ini juga menghadirkan nuansa wisata khas Eropa. Semua itu diterapkan dalam bentuk spot swafoto Instagramable.",
                   textAlign: TextAlign.center,
                   style: TextStyle(fontSize: 16.0),
-                )),
-            Container(
-              padding: const EdgeInsets.all(10.0),
-              child: Column(
-                children: <Widget>[
-                  const Text("ini untuk ke Button"),
+                ),
+              ),
+              SizedBox(
+                height: 150.0,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: Image.network(
+                            'https://media-cdn.tripadvisor.com/media/photo-s/0d/7c/59/70/farmhouse-lembang.jpg'),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: Image.network(
+                            'https://media-cdn.tripadvisor.com/media/photo-w/13/f0/22/f6/photo3jpg.jpg'),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: Image.network(
+                            'https://media-cdn.tripadvisor.com/media/photo-m/1280/16/a9/33/43/liburan-di-farmhouse.jpg'),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              //ini testing sesi
+              Container(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  children: <Widget>[
+                    const Text("ini untuk ke Button"),
+                    ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                      const ButtonDropDown()));
+                        },
+                        child: const Text(("Pindah ke latihan Button")))
+                  ],
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.all(10.0),
+                child: Column(children: [
+                  const Text("Ini latihan input widget"),
                   ElevatedButton(
                       onPressed: () {
                         Navigator.push(
                             context,
                             MaterialPageRoute(
-                                builder: (context) => const ButtonDropDown()));
+                                builder: (context) => const InputWidget()));
                       },
-                      child: const Text(("Pindah ke latihan Button")))
-                ],
+                      child: const Text("pindah ke input widget"))
+                ]),
               ),
-            ),
-            Container(
-              margin: const EdgeInsets.all(10.0),
-              child: Column(children: [
-                const Text("Ini latihan input widget"),
-                ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const InputWidget()));
-                    },
-                    child: const Text("pindah ke input widget"))
-              ]),
-            ),
-            Container(
-              margin: const EdgeInsets.all(10.0),
-              child: Column(
-                children: [
-                  const Text("latihan image"),
-                  ElevatedButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => const LatihanImage())
-                        );
-                      },
-                      child: Text('latihan image'))
-                ],
-              ),
-            )
-          ],
+              Container(
+                margin: const EdgeInsets.all(10.0),
+                child: Column(
+                  children: [
+                    const Text("latihan image"),
+                    ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const LatihanImage()));
+                        },
+                        child: const Text('latihan image'))
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
